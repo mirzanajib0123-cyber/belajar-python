958a0712d188090c90839de54eb730dcebfec227
mirza najibXrpl1 <mirzanajib0123@gmail.com>
2026-08-11 16:51:09 +0700

    Add files via upload

---
+++ bab_2_python.py
@@ -0,0 +1,9 @@
+# ini adalah komentar - tidak di jalankan oleh python
+# progrma: salam perkenalan
+
+print('hallo, dunia...!')
+print('belajar python')
+print('10+5')
+print('jumlah:', 10 + 5)
+print() # kosong
+
