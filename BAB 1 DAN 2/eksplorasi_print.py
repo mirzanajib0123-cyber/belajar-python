69dd2139608d17cd5c9acd698e0d7680f981566f
mirza najibXrpl1 <mirzanajib0123@gmail.com>
2026-09-08 20:08:09 +0700

    Add files via upload

---
+++ BAB 1 DAN 2/eksplorasi_print.py
@@ -0,0 +1,25 @@
+#Percobaan A -- Mencetak Teks dan Angka
+
+print ("Belajar Python menyenangkan!")
+print (100)
+print (3.14)
+print ("Nilai Pi:", 3.14)
+
+
+#Percobaan B -- print() sebagai kalkulator
+
+print (10 + 5)      #penjumlahan
+print (20 - 8)      #pengurangan
+print (4 * 7)       #perkalian
+print (15 / 4)      #pembagian
+print (15 // 4)     #pembagian bulat
+print (15 % 4)      #sisa bagi
+print (2 ** 8)      #perpangkatan
+
+
+#Percobaan C -- Parameter sep dan end
+
+print ("Senin" , "Selasa", "Rabu",)
+print ("Senin" , "Selasa", "Rabu", sep = " | ")
+print ("Baris 1", end = " ")
+print (" Baris 2 di baris yang sama ")
---
+++ BAB 1 DAN 2/progam_pertama.py
@@ -0,0 +1,11 @@
+# progam pertamaku
+# nama: [miraz]
+# kelas: X RPL 1
+
+print("=============================")
+print("   SELAMA DATANG DI PYTHON   ")
+print("=============================")
+print()
+print("progam ini di buat oleh:", "[mirza]")
+print("sekolah: SMK TJP TUBAN")
+print("tahun :", 2026)
