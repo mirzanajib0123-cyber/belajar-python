3b133c6facce8c9f83c2a5f2c19956b5ac8bf2f5
mirza najibXrpl1 <mirzanajib0123@gmail.com>
2026-09-09 17:22:45 +0700

    Delete bab_2_python.py

--- bab_2_python.py
+++
@@ -1,9 +0,0 @@
-# ini adalah komentar - tidak di jalankan oleh python
-# progrma: salam perkenalan
-
-print('hallo, dunia...!')
-print('belajar python')
-print('10+5')
-print('jumlah:', 10 + 5)
-print() # kosong
-
